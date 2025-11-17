ffc65e366cd1e43cbc3812b0bdb2c44220a57594
MXJXN <modulations@gmail.com>
2025-11-16 21:18:50 -0500

    docs site nextjs basepath uncommented

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     unoptimized: true,
   },
   // GitHub Pages base path (will be /such-lssvm for the repo)
-  // basePath: '/such-lssvm', // Uncomment when deploying to GitHub Pages
+  basePath: '/such-lssvm', // Uncomment when deploying to GitHub Pages
   trailingSlash: true,
 }
 
